acd561436034b3edd34fceccc13db72dd5c698d9
tonny0831 <tonnyjansen0831@gmail.com>
2022-06-23 14:27:57 +0200

    test: :white_check_mark: add `solana` tests based on `ava` (#25)

--- contracts/SPL/tests/puppet.test.ts
+++ contracts/SPL/tests/puppet.test.ts
@@ -1,38 +1,39 @@
-const assert = require("assert");
-const anchor = require("@project-serum/anchor");
-const { SystemProgram } = anchor.web3;
+import undefinedtest, { TestFn } from "ava";
+import * as anchor from "@project-serum/anchor";
 
-describe("basic-3", () => {
-  const provider = anchor.AnchorProvider.local();
+const test = undefinedtest as TestFn<{}>;
 
-  // Configure the client to use the local cluster.
+test("Performs CPI from puppet master to puppet", async (t) => {
+  const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
-  it("Performs CPI from puppet master to puppet", async () => {
-    const puppetMaster = anchor.workspace.PuppetMaster;
-    const puppet = anchor.workspace.Puppet;
+  const puppetMaster = anchor.workspace.PuppetMaster;
+  const puppet = anchor.workspace.Puppet;
 
-    // Initialize a new puppet account.
-    const newPuppetAccount = anchor.web3.Keypair.generate();
-    const tx = await puppet.rpc.initialize({
-      accounts: {
-        puppet: newPuppetAccount.publicKey,
-        user: provider.wallet.publicKey,
-        systemProgram: SystemProgram.programId,
-      },
-      signers: [newPuppetAccount],
-    });
+  // Initialize a new puppet account.
+  const newPuppetAccount = anchor.web3.Keypair.generate();
 
-    // Invoke the puppet master to perform a CPI to the puppet.
-    await puppetMaster.rpc.pullStrings(new anchor.BN(111), {
-      accounts: {
-        puppet: newPuppetAccount.publicKey,
-        puppetProgram: puppet.programId,
-      },
-    });
-
-    // Check the state updated.
-    puppetAccount = await puppet.account.data.fetch(newPuppetAccount.publicKey);
-    assert.ok(puppetAccount.data.eq(new anchor.BN(111)));
+  const tx = await puppet.rpc.initialize({
+    accounts: {
+      puppet: newPuppetAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    },
+    signers: [newPuppetAccount],
   });
+
+  // Invoke the puppet master to perform a CPI to the puppet.
+  await puppetMaster.rpc.pullStrings(new anchor.BN(111), {
+    accounts: {
+      puppet: newPuppetAccount.publicKey,
+      puppetProgram: puppet.programId,
+    },
+  });
+
+  // Check the state updated.
+  const puppetAccount = await puppet.account.data.fetch(
+    newPuppetAccount.publicKey
+  );
+
+  t.true(puppetAccount.data.eq(new anchor.BN(111)));
 });
